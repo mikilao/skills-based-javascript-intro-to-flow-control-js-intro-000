6a21e3bd2db4ed52e3a6ac7d2a5eaae2deffa128
Rachel S <queenuvchaos@gmail.com>
2019-10-11 23:17:40 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -14,7 +14,13 @@ function teenager(age) {
 }
 
 function ageChecker(age) {
-
+  if(age <= 19 && age >= 13) {
+    return "You are a teenager!"
+  }else if(age <= 12){
+    return "You are a kid"
+  }else{
+    return "You are an adult"
+  }
 }
 
 function ternaryTeenager(age) {
